a84b4115d05085fdf593f166c11631bcbce0c8fd
GeekCorner <45696571+GeekCornerGH@users.noreply.github.com>
2022-01-16 18:29:51 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const strings = require("customStrings.json");
+let result;
+fs.readFile("./customStrings.json", 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  strings.forEach(string => {
+    result = data.replace(string.original, string.new);
+  }
+
+  fs.writeFile("./customStrings.json", result, 'utf8', function (err) {
+     if (err) return console.log(err);
+  });
+});
