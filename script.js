897a771b02d62b6f1d84d50e10c9f662c6f75bd0
GeekCorner <45696571+GeekCornerGH@users.noreply.github.com>
2022-01-17 11:17:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const strings = require("./customStrings.json");
 let result;
 strings.forEach(async string => {
   console.log("String actuelle: " + JSON.stringify(string));
-  await fs.readFile("./messages.properties", 'utf8', function (err,data) {
+  await fs.readFile("./messages.properties", 'utf8', async function (err,data) {
   if (err) {
     return console.log(err);
   }
-  result = data.replace(string.original, string.new);
+  result = await data.replace(string.original, string.new);
 
-  fs.writeFile("./messages.properties", result, 'utf8', function (err) {
+  await fs.writeFile("./messages.properties", result, 'utf8', function (err) {
      if (err) return console.log(err);
   });
 });
