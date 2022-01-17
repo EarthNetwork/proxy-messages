ab835a727f0b789b2382d5fcb6d4ec22b82009c5
GeekCorner <45696571+GeekCornerGH@users.noreply.github.com>
2022-01-17 12:00:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ fs.readFile('./customStrings.json', 'utf8', (err, strings) => {
         if (err) {
             return console.log(err);
         }
-        original.forEach(async string => {
+        JSON.parse(original).forEach(async string => {
             console.log("String actuelle: " + JSON.stringify(string));
             result = await data.replaceAll(new RegExp(string.original, 'g'), string.new);
             await fs.writeFile("./messages.properties", result, 'utf8', function (err) {
