2c28d312ffdbe1f1f371ece2e8581ff81a2d63de
GeekCorner <45696571+GeekCornerGH@users.noreply.github.com>
2022-01-17 11:56:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const fs = require("fs");
-const strings = require("./customStrings.json").replaceAll("\\\\","\\\\\\\\");
-let result;
-  fs.readFile("./messages.properties", 'utf8', async function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-    strings.forEach(async string => {
-        console.log("String actuelle: " + JSON.stringify(string));
-  result = await data.replaceAll(new RegExp(string.original, 'g'), string.new);
-      await fs.writeFile("./messages.properties", result, 'utf8', function (err) {
-     if (err) return console.log(err);
-  });
-      console.log(result)
-      });
+fs.readFile('./customStrings.json', 'utf8', (err, strings) => {
+    const orignal = strings.replaceAll("\\\\", "\\\\\\\\");
+    let result;
+    fs.readFile("./messages.properties", 'utf8', async function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        original.forEach(async string => {
+            console.log("String actuelle: " + JSON.stringify(string));
+            result = await data.replaceAll(new RegExp(string.original, 'g'), string.new);
+            await fs.writeFile("./messages.properties", result, 'utf8', function (err) {
+                if (err) return console.log(err);
+            });
+            console.log(result)
+        });
 
-});
-
+    });
+})
